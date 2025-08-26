3388c204bd4b4af2db9ba48bfa3c5d80f4939339
AmaPrabuddi <164039842+AmaPrabuddi@users.noreply.github.com>
2025-08-26 18:01:26 +0530

    Explore Magazine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Team from "./components/Team";
 import Contact from "./components/Contact";
+import MagazineCard from './components/MagazineCard';
+import Events from "./components/Events";
 import Footer from "./components/Footer";
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
       <Header/>
       <Hero/>
       <About/>
+      <MagazineCard/>
       <Team/>
       <Contact/>
       {/* <Footer/> */}
